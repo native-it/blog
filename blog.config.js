a4d8eae2ca720ffa6ca96a708f94c1c933a9ff5f
AndrejVidovic <vidaftw@gmail.com>
2024-01-24 15:49:06 +0100

    Keywords, default dark

--- blog.config.js
+++ blog.config.js
@@ -6,7 +6,7 @@ const BLOG = {
   description: 'Mobile Development Experts',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Europe/Zagreb', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
-  appearance: 'auto', // ['light', 'dark', 'auto'],
+  appearance: 'dark', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#010001', // use hex value, don't forget '#'
@@ -20,7 +20,7 @@ const BLOG = {
   ogImageGenerateURL: 'https://native-it.com/assets/native_facebook.png', // The link to generate OG image, don't end with a slash
   socialLink: 'https://native-it.com/',
   seo: {
-    keywords: ['Blog', 'Website', 'Native'],
+    keywords: ['Blog', 'Website', 'Native','Swift', 'Kotlin', 'MultiPlatform','iOS', 'Android', 'PHP', 'Symfony', 'SwiftUI', 'Compose'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
